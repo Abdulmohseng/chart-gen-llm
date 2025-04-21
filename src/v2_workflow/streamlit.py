ffcfc29eb4c5bfdd8ae931ccce2c4b6df6c50699
Abdulmohseng <epiic.ag@gmail.com>
2025-04-21 16:24:48 +0300

    file uploader from user

--- src/v2_workflow/streamlit.py
+++ src/v2_workflow/streamlit.py
@@ -4,6 +4,7 @@ from state import State
 from main import build_graph, invoke_build_graph
 from langgraph.types import Command
 import plotly.io as pio
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
@@ -17,6 +18,9 @@ if "graph" not in st.session_state:
 def get_state():
     return st.session_state.graph.get_state(thread_config)
 
+# if "state" not in st.session_state:
+#     st.session_state.state = get_state()
+
 datasets = {"China Vs Japan": "data/japanvchina.csv",
             "50 Start ups": "data/50_Startups.csv",
             "Student Performance": "data/StudentsPerformance.csv",
@@ -25,41 +29,63 @@ datasets = {"China Vs Japan": "data/japanvchina.csv",
 # st.text(graph.get_state(thread_config))
 # st.write(graph)
 
-
+if "file_name" not in st.session_state:
+    st.session_state.file_name = None
+if "df" not in st.session_state:
+    st.session_state.df = None
 
 # st.text(st.session_state.graph.get_state(thread_config))
 # graph.invoke(Command(resume="data/japanvchina.csv"), config=thread_config)
 # st.text(graph.get_state(thread_config).tasks[0].interrupts)
 # graph.invoke(Command(resume="bar chart comparing tech sectors japan v china"), config=thread_config)
 # st.code(graph.get_state(thread_config).values['code'])
+# state_placeholder = st.empty()
+# state_placeholder.write(get_state())
 col1, col2 = st.columns([1,2])
-with col1:
-    if get_state().tasks[0].interrupts:
-        with st.form("input_form", clear_on_submit=True):
-            text_placeholder = st.empty()
-            user_input = st.text_area("", key="user_input")
-            submitted = st.form_submit_button("Submit")
-            text_placeholder.markdown("### " +get_state().tasks[0].interrupts[0].value)
-        if submitted:
-            with st.spinner("Please wait ..."):
-                st.session_state.graph.invoke(Command(resume=user_input), config=thread_config)
-            text_placeholder.text(get_state().tasks[0].interrupts[0].value)
-        if get_state().values['business_questions']:
-            st.subheader("Business questions and charts:", divider="gray")
-            st.markdown(get_state().values['business_questions'])
-        else:
-            st.subheader("Available datasets: ", divider="gray")
-            st.json(datasets)
+
+if get_state().next[0] == "input_dataset":
+    upload_file = st.file_uploader("Please upload a datasets: ")
+    if upload_file:
+        st.session_state.file_name = "data/"+upload_file.name
+        # st.write(st.session_state.file_name)
+        st.session_state.df = pd.read_csv(st.session_state.file_name)
         
-with col2:
-    if get_state().values['figures']:
-        st.plotly_chart(pio.from_json(get_state().values['figures']['llm_generated_plot']))
-    # if get_state().values['code']:
-        st.code(get_state().values['code'])
-    elif get_state().values['summary']:
-        st.subheader("Input data summary:", divider="gray")
-        st.markdown(get_state().values['summary'])
-    
+        st.session_state.graph.invoke(Command(resume=st.session_state.file_name), config=thread_config)
+        st.rerun()
+else:
+    with col1:
+        # if not get_state().values["summary"]:
+        if get_state().tasks[0].interrupts:
+            with st.form("input_form", clear_on_submit=True):
+                st.dataframe(st.session_state.df.head())
+                text_placeholder = st.empty()
+                user_input = st.text_area("", key="user_input")
+                submitted = st.form_submit_button("Submit")
+                text_placeholder.markdown("### " + get_state().tasks[0].interrupts[0].value)
+
+            if submitted:
+                with st.spinner("Please wait ..."):
+                    st.session_state.graph.invoke(Command(resume=user_input), config=thread_config)
+                text_placeholder.text(get_state().tasks[0].interrupts[0].value)
+                # state_placeholder.write(get_state())
+
+            if get_state().values['business_questions']:
+                st.subheader("Business questions and charts:", divider="gray")
+                st.markdown(get_state().values['business_questions'])
+            else:
+                st.subheader("Available datasets: ", divider="gray")
+                st.json(datasets)
+            
+    with col2:
+        if get_state().values['figures']:
+            st.plotly_chart(pio.from_json(get_state().values['figures']['llm_generated_plot']))
+        # if get_state().values['code']:
+            st.code(get_state().values['code'])
+        elif get_state().values['summary']:
+            st.subheader("Input data summary:", divider="gray")
+            st.markdown(get_state().values['summary'])
+
+st.write(get_state())
 # st.write(user_input)
 # st.rerun()
 # st.rerun()
--- src/v2_workflow/user_interaction.py
+++ src/v2_workflow/user_interaction.py
@@ -3,7 +3,7 @@ from langgraph.types import interrupt
 
 def user_chart_selection(state: State):
     print("---Step 3: user feedback---")
-    choice = interrupt("Choose a chart to create: ") # or "Line chart with Year on the x-axis, Market Share (%) on the y-axis, and separate lines for each Tech Sector, further grouped by Country (Japan, China). A dual-axis chart could be considered if the market share scales are vastly different between countries."
+    choice = interrupt("Prompt LLM with your chart description: ") # or "Line chart with Year on the x-axis, Market Share (%) on the y-axis, and separate lines for each Tech Sector, further grouped by Country (Japan, China). A dual-axis chart could be considered if the market share scales are vastly different between countries."
     return {"chart_selected": choice}
 
 def user_change_request(state: State):
